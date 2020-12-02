3c15efec36329dcb4e4057f12ef5f1767a9a9c2f
Jason R <99057964@mydavinci.nl>
2020-12-02 14:42:18 +0100

    Bezig met de fly functie.

--- Duckhunt/duckhunt.js
+++ Duckhunt/duckhunt.js
@@ -1,9 +1,21 @@
 //Duck Hunt.
 //01-12-2020
 
+var direction = ['N,S,W,E'];//Array voor de richtingen.
+var duck = document.getElementById('duck');//Pakt de id van de duck GIF.
+var top = 250;
+var bottom = 250;
+
 function fly(direction){
-    direction = document.getElementById('duckGif');
+    var random = Math.floor(Math.random() * 4);
+    var direction = direction[random];
+    if(direction == "N"){
+        top = top + 50;
+        duck.style.top = top + "px";
+    } else {
+        alert('Hoi');
+    }
 
 }
 
-fly();
+fly();//Functie voor het vliegen.
