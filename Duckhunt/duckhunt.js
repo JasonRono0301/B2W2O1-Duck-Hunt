a7e7d31e65eeb52acbe94436aaee6340bfe697e2
Jason R <99057964@mydavinci.nl>
2020-12-04 12:56:03 +0100

    Bezig met het bewegen van de duck.

--- Duckhunt/duckhunt.js
+++ Duckhunt/duckhunt.js
@@ -1,21 +1,28 @@
 //Duck Hunt.
 //01-12-2020
 
-var direction = ['N,S,W,E'];//Array voor de richtingen.
-var duck = document.getElementById('duck');//Pakt de id van de duck GIF.
-var top = 250;
-var bottom = 250;
+var stage = document.getElementById('stageButton');//De manupilatie voor de stage.
+var duck = document.getElementById('duckGif');//De manupilatie voor de duck button.
+var directions = ["north","south","east","west"];//Array voor de richtingen.
+var posTop = 450;
+var posRight = 1000; 
 
 function fly(direction){
     var random = Math.floor(Math.random() * 4);
-    var direction = direction[random];
-    if(direction == "N"){
-        top = top + 50;
-        duck.style.top = top + "px";
-    } else {
-        alert('Hoi');
+    var direction = directions[random];//Pakt een random index waarde uit de array.
+    console.log(direction);//Laat in de console log zien welk index waarde heeft gepakt.
+
+    if(direction == "north"){
+        posTop = posTop + 50;
+        duck.style.top = posTop + "px";
+    } 
+    else if(direction == "east"){
+        posRight = posRight + 100;
+        duck.style.left = posRight + "px";
+
     }
 
-}
+}   
 
-fly();//Functie voor het vliegen.
+fly();//Roept de (fly) functie aan.
+
