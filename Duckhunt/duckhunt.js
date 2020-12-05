3a788935979f922149fc4128064e49fa8511a36f
Jason R <99057964@mydavinci.nl>
2020-12-05 18:37:29 +0100

    De beweging van de duck gemaakt.
    Heb de duck beweging gemaakt van boven en naar beneden en het is werkt.

--- Duckhunt/duckhunt.js
+++ Duckhunt/duckhunt.js
@@ -4,25 +4,37 @@
 var stage = document.getElementById('stageButton');//De manupilatie voor de stage.
 var duck = document.getElementById('duckGif');//De manupilatie voor de duck button.
 var directions = ["north","south","east","west"];//Array voor de richtingen.
-var posTop = 450;
-var posRight = 1000; 
+var posTop = 250;//Variabele met daarin een number die zich als pixels bevind. Zodat de Gif van zijn standaard positie veranders zodra er iets bij komt of eraf.
+var posRight = 500;//Zie regel 7!.
 
-function fly(direction){
-    var random = Math.floor(Math.random() * 4);
+function fly(direction){//De code voor de fly functie.
+    var random = Math.floor(Math.random() * 4);//Pakt een nummer tussen 0 en 4.
     var direction = directions[random];//Pakt een random index waarde uit de array.
     console.log(direction);//Laat in de console log zien welk index waarde heeft gepakt.
 
     if(direction == "north"){
-        posTop = posTop + 50;
+        if(posTop > 0){
+            posTop = posTop - 50;
+        }
+        else {
+            posTop = 250;
+        }
         duck.style.top = posTop + "px";
-    } 
-    else if(direction == "east"){
-        posRight = posRight + 100;
-        duck.style.left = posRight + "px";
-
     }
 
-}   
+    if(direction  == "south"){
+        if(posTop < 600){
+            posTop = posTop + 50;
+        }
+        else {
+            posTop = 250;
+        }
+        duck.style.top = posTop + "px";
+    }
 
-fly();//Roept de (fly) functie aan.
+};
+
+//Roept de (fly) functie aan.
+//var timer = setInterval(fly,1000);
+
 
